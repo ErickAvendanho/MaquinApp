e832a0f7fc7d30ff959d4644daefcc58831c7bab
RaulGonzalezIrgtec <raul.gonzalez@igrtec.com>
2022-07-26 17:10:19 -0500

    Corrección en sesión de usuario

--- maquinapp/lib/main.dart
+++ maquinapp/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:maquinapp/Pages/home/home_page_signin.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:maquinapp/Pages/home/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   WidgetsFlutterBinding
@@ -29,7 +31,30 @@ class _MyAppState extends State<MyApp> {
       title: 'Maquinapp',
       debugShowCheckedModeBanner: false,
       initialRoute: 'Home',
-      routes: {'Home': (BuildContext context) => const HomePageSignIn()},
+      routes: {
+        'Home': (BuildContext context) => StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, stream) {
+                if (stream.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child:
+                            Image.asset('assets/images/wellnesstar-logo.jpeg'),
+                      ),
+                      const CircularProgressIndicator(),
+                    ],
+                  );
+                } else if (stream.hasData) {
+                  return const HomePage();
+                } else {
+                  return const HomePageSignIn();
+                }
+              },
+            ),
+      },
     );
   }
 }
